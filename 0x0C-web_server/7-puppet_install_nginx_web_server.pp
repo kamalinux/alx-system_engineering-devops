c2261bc63c5619499ef5b9f7bb9f83447ab70605
Kamal Hussein <kamalinuxx@gmail.com>
2022-12-13 10:04:12 -0800

    Fixing sed command

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -2,5 +2,5 @@
 
 exec {'install':
   provider => shell,
-  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "46i \\\n\tlocation /redirect_me {\n\t\t return 301 https://github.com/kamalinux;\n\t}\n" /etc/nginx/sites-available/default ; sudo service nginx start',
+  command  => 'sudo apt-get -y update ; sudo apt-get -y install nginx ; echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html ; sudo sed -i "46i \\\\\n\tlocation /redirect_me {\n\t\t return 301 https://github.com/kamalinux;\n\t}\n" /etc/nginx/sites-available/default ; sudo service nginx start',
 }
