3126767ebd1051cba647aa9642c79e556c3e6311
Kamal Hussein <kamalinuxx@gmail.com>
2023-01-16 19:58:19 +0000

    Extend my Python script to export data in the CSV format.

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Rn TODO list progress given employee ID
+Export this data to CSV
+"""
+import csv
+import requests
+import sys
+
+
+def to_csv():
+    """return API data"""
+    user_id = sys.argv[1]
+    url = "https://jsonplaceholder.typicode.com/"
+    user = requests.get(url + "users/{}".format(user_id)).json()
+    username = user.get("username")
+    todos = requests.get(url + "todos", params={"userId": user_id}).json()
+
+    with open("{}.csv".format(user_id), "w", newline="") as csvfile:
+        writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+        [writer.writerow(
+            [user_id, username, t.get("completed"), t.get("title")]
+         ) for t in todos]
+
+
+if __name__ == "__main__":
+    to_csv()
